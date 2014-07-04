18865c6b2af5eb6953054f02eeac8fa4d6d5b1ae
Forrest L Norvell <forrest@npmjs.com>
2014-07-04 12:12:46 -0700

    fleshed out console example

--- problems/console/index.js
+++ problems/console/index.js
@@ -1,5 +1,7 @@
 var readFile = require("graceful-fs").readFile;
 var resolve = require("path").resolve;
+var spawn = require("win-spawn");
+var inspect = require("util").inspect;
 
 module.exports = function () {
   return {
@@ -18,13 +20,64 @@ module.exports = function () {
     },
 
     verify : function verify(args, t) {
-      var filename = args[0];
-      if (!filename) {
-        t.fail("must include file to verify");
+      var testerPath = args[0];
+      if (!testerPath) {
+        t.fail("must include program to verify");
         return t.end();
       }
 
-      // TODO: stuff here
+      var jsonPath = resolve(__dirname, "./mushroom-kingdom.json");
+      var scenarioPath = resolve(process.cwd(), testerPath);
+
+      var failer = spawn("node", [scenarioPath]);
+
+      var failout = "", failerr = "";
+      failer.stdout.on("data", function (data) {
+        failout += data.toString("utf8");
+      });
+
+      failer.stderr.on("data", function (data) {
+        failerr += data.toString("utf8");
+      });
+
+      failer.on("close", function () {
+        t.ok(failerr.match(/bowser/, "found error stack"));
+        t.notOk(failout, "no standard output");
+
+        var succeeder = spawn("node", [scenarioPath, jsonPath]);
+
+        var successout = "", successerr = "";
+        succeeder.stdout.on("data", function (data) {
+          successout += data.toString("utf8");
+        });
+
+        succeeder.stderr.on("data", function (data) {
+          successerr += data.toString("utf8");
+        });
+
+        succeeder.on("close", function (code) {
+          t.equal(code, 0, "program executed successfully");
+          if (!successout) {
+            t.fail("no standard output");
+            return t.end();
+          }
+
+          t.equals(successerr.indexOf("Trace: traced"), 0, "found evidence of trace 1");
+          t.ok(successerr.match(/peach/), "found evidence of trace 2");
+          t.ok(successerr.match(/koopa/), "found evidence of trace 3");
+
+          // t.doesNotThrow(function () {
+            var baseline = require("./mushroom-kingdom.json");
+            t.deepEqual(
+              successout.trim(),
+              inspect(baseline),
+              "got the expected JSON"
+            );
+          // }, "JSON parsed OK");
+
+          t.end();
+        });
+      });
     }
   };
 };
---
+++ problems/console/solution.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+
+var peach = function (obj) {
+  console.trace("traced");
+  console.log(obj);
+};
+
+var bowser = function (callback) {
+  fs.readFile(process.argv[2], {encoding : "utf8"}, callback);
+};
+
+var koopa = function (err, file) {
+  if (err) return console.error("Handle your errors folks.");
+
+  peach(JSON.parse(file));
+};
+
+bowser(koopa);
