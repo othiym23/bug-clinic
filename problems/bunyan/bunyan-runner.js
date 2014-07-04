5156a724673d93b47ef04bed6a2addc29f2f290e
Forrest L Norvell <forrest@npmjs.com>
2014-07-04 15:13:40 -0700

    tweak problem statement, add bunyan-runner

---
+++ problems/bunyan/bunyan-runner.js
@@ -0,0 +1,9 @@
+var resolve = require("path").resolve;
+
+var bunyan = require("bunyan");
+var log = bunyan.createLogger({name : "sample"});
+
+var scenario = require(resolve(process.cwd(), process.argv[2]));
+scenario(log, function (value) {
+  log.info("value is", value);
+});
