715804b32d0da8bb5737a1cf754e9e4bc882342c
Forrest L Norvell <forrest@npmjs.com>
2014-07-07 12:32:00 -0700

    wording tweak

--- problems/bunyan/bunyan-runner.js
+++ problems/bunyan/bunyan-runner.js
@@ -5,5 +5,5 @@ var log = bunyan.createLogger({name : "sample"});
 
 var scenario = require(resolve(process.cwd(), process.argv[2]));
 scenario(log, function (value) {
-  log.info("value is", value);
+  log.info("value at finish is", value);
 });
