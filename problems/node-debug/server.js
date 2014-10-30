4c1042fb2fa1d583ae25caa90d39c55b54ec3984
Kenan Shifflett <kenan@spanishdict.com>
2014-10-29 22:49:44 -0400

    Add server error handler. Fail if address is already in use.

--- problems/node-debug/server.js
+++ problems/node-debug/server.js
@@ -1,3 +1,7 @@
 var createServer = require("http").createServer;
 var server = createServer(function (req, res) { res.end("hello"); });
+server.on("error", function (err) {
+  console.error(err.stack);
+  process.exit(1);
+});
 server.listen(9876, function () { console.log("listening"); });
